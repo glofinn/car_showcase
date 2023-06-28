b3f989b40b8b593698bf0c023c868b719715a9d2
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-06-28 16:58:52 +0200

    hero layout complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,7 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
         </div>
       </div>
     </div>
